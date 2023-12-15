fde99b7974cd99bd7bbdd705acfbdaf90df94bd9
praset911 <117353296+praset911@users.noreply.github.com>
2023-12-16 01:57:34 +0700

    Update API.js

--- src/services/API.js
+++ src/services/API.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-const getHealthNews = async () => {
+const getNewsData = async () => {
   try {
     const response = await axios.get(
-      `https://saurav.tech/NewsAPI/top-headlines/category/health/in.json`
+      `https://berita-indo-api-next.vercel.app/api/suara-news/health`
     );
-    return response.data.articles;
+    return response.data.data || [];
   } catch (error) {
-    console.error('Error fetching health news:', error);
+    console.error('Error fetching news data:', error);
     return [];
   }
 };
 
-export default getHealthNews;
+export default getNewsData;
