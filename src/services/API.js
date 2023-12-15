d731249c82ec2222bff54a092a44a6bd72ebffff
praset911 <117353296+praset911@users.noreply.github.com>
2023-12-15 22:57:38 +0700

    Update API.js

--- src/services/API.js
+++ src/services/API.js
@@ -1,16 +1,9 @@
 import axios from 'axios';
 
-const apiKey = '224fdd7405614aa3a515d6d61f3efa75';
-
 const getHealthNews = async () => {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=${apiKey}`,
-      {
-        headers: {
-          'Upgrade-Insecure-Requests': '1',
-        },
-      }
+      `https://saurav.tech/NewsAPI/top-headlines/category/health/in.json`
     );
     return response.data.articles;
   } catch (error) {
